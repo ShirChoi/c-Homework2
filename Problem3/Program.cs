5f0a4a1beabf43a987f65fdfd7c4833e11c9eec8
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-05 23:02:21 +0500

    +problem3

---
+++ Problem3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem3 {
+    class Program {
+        static void Main(string[] args) {
+            int num = ReadInt("number");
+
+            string result = "";
+
+            if(num < 0 || num > 100) {
+                result = "число должно находится в промежутке 0-100";
+            } else if(num <= 14) {
+                result = "[0 - 14]";
+            } else if (num <= 35) {
+                result = "[15 - 35]";
+            } else if (num <= 50) {
+                result = "[36 - 50]";
+            } else if (num <= 100) {
+                result = "[51 - 100]";
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static int ReadInt(string varName) {
+            Console.Write(varName + " = ");
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
