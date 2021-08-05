664ca5f5a7d754bb544679c3f89269ac783a4b95
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-05 22:51:33 +0500

    first 2 problems

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace Problem1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            int A = ReadInt("A");
+            int B = ReadInt("B");
+
+            if(A != B) {
+                A = Math.Max(A, B);
+                B = Math.Max(A, B);
+            } else {
+                A = B = 0;
+            }
+
+            Console.WriteLine($"A = {A}");
+            Console.WriteLine($"B = {B}");
+        }
+
+        static int ReadInt(string varName) {
+            Console.Write(varName + " = ");
+
+            return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
---
+++ Problem2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Problem2 {
+    class Program {
+        static void Main(string[] args) {
+            double operand1 = ReadDouble("operand1");
+            double operand2 = ReadDouble("operand2");
+
+            Console.Write("sign = ");
+            string sign = Console.ReadLine();
+
+            double result;
+
+            switch (sign) {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    if(operand2 == 0) {
+                        System.Console.WriteLine("нельзя делить на ноль");
+
+                        return;
+                    }
+
+                    result = operand1 / operand2;
+                    break;
+                default:
+                    System.Console.WriteLine($"операция {sign} не поддерживается");
+                    return;
+                    break;
+            }
+
+            System.Console.WriteLine($"result = {result}");
+        }
+
+        static double ReadDouble(string varName) {
+            Console.Write(varName + " = ");
+
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
